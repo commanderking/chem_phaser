2049cb183099c47f5d0d5cf4a743c1c545abf9b6
commanderking <jek582@mail.harvard.edu>
2019-01-20 15:52:07 -0500

    refactor all atoms and text into containers to render

--- js/constants/atomConstants.js
+++ js/constants/atomConstants.js
@@ -1,17 +1,35 @@
 const elements = {
   SODIUM: {
     key: "sodium",
+    symbol: "Na",
+    atomColor: 0xffffff,
     diameter: 50,
-    valenceElectrons: 1
+    valenceElectrons: 1,
+    initPosition: {
+      x: 100,
+      y: 300
+    }
   },
   CHLORINE: {
     key: "chlorine",
+    symbol: "Cl",
+    atomColor: 0xff0000,
     diameter: 100,
-    valeneceElectrons: 7
+    valeneceElectrons: 7,
+    initPosition: {
+      x: 500,
+      y: 500
+    }
   },
   FLUORINE: {
     key: "fluorine",
+    symbol: "F",
+    atomColor: 0x9400d3,
     diameter: 75,
-    valenceElectrons: 7
+    valenceElectrons: 7,
+    initPosition: {
+      x: 200,
+      y: 200
+    }
   }
 };
--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
 const { SODIUM, CHLORINE, FLUORINE } = elements;
 
-var state = {
-  atomX: null,
-  atomY: null
-};
-
 let atomToTextMap = {};
 
 var config = {
@@ -27,6 +22,7 @@ var game = new Phaser.Game(config);
 var text;
 function preload() {}
 function create() {
+  // Creates graphic and texture for atom, then deletes graphic
   const renderAtom = ({ name, color, diameter }) => {
     const atom = new Phaser.Geom.Ellipse(
       diameter / 2,
@@ -39,94 +35,54 @@ function create() {
       .graphics({ fillStyle: { color } })
       .fillEllipseShape(atom, 64);
 
-    atomGraphics.generateTexture(name, diameter, diameter);
+    const texture = atomGraphics.generateTexture(name, diameter, diameter);
     atomGraphics.destroy();
+    return texture;
   };
 
-  const chlorine = renderAtom({
-    name: "chlorine",
-    color: 0xff0000,
-    diameter: 100
-  });
+  // Renders Atom texture, text, and makes container draggable
+  const renderAtomContainer = ({
+    key,
+    atomColor,
+    diameter,
+    symbol,
+    initPosition
+  }) => {
+    const { x, y } = initPosition;
+    const container = this.add.container(x, y);
+    container.key = `${key}Container`;
 
-  const fluorine = renderAtom({
-    name: "fluorine",
-    color: 0x9400d3,
-    diameter: 75
-  });
+    const atom = renderAtom({
+      name: key,
+      color: atomColor,
+      diameter
+    });
+    const sodiumSprite = this.add.sprite(0, 0, key);
 
-  const sodium = renderAtom({
-    name: "sodium",
-    color: 0xffffff,
-    diameter: SODIUM.diameter
-  });
+    const containerText = this.add.text(0, 0, symbol, {
+      font: "bold 20px Arial",
+      color: "green"
+    });
+    containerText.setOrigin(0.5, 0.5);
 
-  this.atoms = this.add.group([
-    {
-      key: "sodium",
-      setXY: {
-        x: 100,
-        y: 300
-      }
-    },
-    {
-      key: "fluorine",
-      setXY: {
-        x: 200,
-        y: 200
-      }
-    },
-    {
-      key: "chlorine",
-      setXY: {
-        x: 300,
-        y: 300
-      }
-    }
-  ]);
+    container.add(sodiumSprite);
+    container.add(containerText);
 
-  atomToTextMap[CHLORINE.key] = this.make.text(
-    createAtomTextConfig({ text: "Cl", x: 300, y: 300 })
-  );
-  atomToTextMap[SODIUM.key] = this.make.text(
-    createAtomTextConfig({ text: "Na", x: 100, y: 300 })
-  );
+    // Making the container draggable
+    container.setInteractive(
+      new Phaser.Geom.Circle(0, 0, diameter / 2),
+      Phaser.Geom.Circle.Contains
+    );
+    this.input.setDraggable(container);
+    container.on("drag", function(pointer, dragX, dragY) {
+      container.x = dragX;
+      container.y = dragY;
+    });
 
-  atomToTextMap[FLUORINE.key] = this.make.text(
-    createAtomTextConfig({ text: "F", x: 200, y: 200 })
-  );
+    return container;
+  };
 
-  Phaser.Actions.Call(
-    this.atoms.getChildren(),
-    function(item) {
-      item.setInteractive();
-      this.input.setDraggable(item);
-      item.on("drag", function(pointer, dragX, dragY) {
-        console.log("item", item.texture.key);
-        console.log("atomTextMap", atomToTextMap);
-        console.log(atomToTextMap[item.texture.key]);
-        item.x = dragX;
-        item.y = dragY;
-
-        atomToTextMap[item.texture.key].x = dragX;
-        atomToTextMap[item.texture.key].y = dragY;
-        // secondText.x = dragX;
-        // secondText.y = dragY;
-
-        state.atomX = dragX;
-        state.atomY = dragY;
-      });
-    },
-    this
-  );
-
-  var text = this.add.text(200, 100, "Sample Text", {
-    font: "bold 64px Arial",
-    align: "center",
-    color: "white",
-    align: "center"
-  });
-  text.setOrigin(0.5);
-  text.alpha = 1;
-  text.visible = 1;
+  renderAtomContainer(CHLORINE);
+  renderAtomContainer(FLUORINE);
+  renderAtomContainer(SODIUM);
 }
